5e74f2629d6a7001882fb0585e892d2d3d946fbe
sonalisonawane <66239376+sonalisonawane@users.noreply.github.com>
2021-03-03 23:15:00 -1100

    file created

---
+++ app.js
@@ -0,0 +1,10 @@
+let firstPartner = document.querySelector(".name-input1");
+let secondPartner = document.querySelector(".name-input2");
+let btnEl = document.querySelector("#btn");
+var percentage = 0;
+
+btnEl.addEventListener("click", function() {
+    let userValueOne = firstPartner.value;
+    let userValueTwo = secondPartner.value;
+
+});

82c50bf5ffa85ef81d37fc0e5157f62af89fa7b8
sonalisonawane <66239376+sonalisonawane@users.noreply.github.com>
2021-03-05 01:10:52 -1100

    Add some Conditions for calculations

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 let firstPartner = document.querySelector(".name-input1");
 let secondPartner = document.querySelector(".name-input2");
+let containerEl = document.querySelector(".container");
+console.log(containerEl)
 let btnEl = document.querySelector("#btn");
 var percentage = 0;
+var score = 0;
+
 
 btnEl.addEventListener("click", function() {
     let userValueOne = firstPartner.value;
     let userValueTwo = secondPartner.value;
-    console.log(userValueOne, userValueTwo)
+    var randomInt = Math.floor(Math.random() * 20);
+    if (userValueOne.length === userValueTwo.length) {
+        score += 5;
+    }
+
+    calculation = randomInt * score;
+    console.log(randomInt, calculation)
+    containerEl.innerHTML.appendChild = "Yours Love is :" + calculation;
 })

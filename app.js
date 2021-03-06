e30c3c6deb94e0f07dfc2124bc46cc2fd6ce61e5
sonalisonawane <66239376+sonalisonawane@users.noreply.github.com>
2021-03-06 02:16:27 -1100

    add display calculation

--- app.js
+++ app.js
@@ -8,14 +8,20 @@ var score = 0;
 
 
 btnEl.addEventListener("click", function() {
-    let userValueOne = firstPartner.value;
-    let userValueTwo = secondPartner.value;
+    let userValueOne = toString(firstPartner.value);
+    let userValueTwo = toString(secondPartner.value);
     var randomInt = Math.floor(Math.random() * 20);
+    let vowels = ["a", "e", "i", "o", "u"]
     if (userValueOne.length === userValueTwo.length) {
         score += 5;
     }
-
+    if (userValueOne.includes(vowels)) {
+        score += 2;
+        console.log(score)
+    }
     calculation = randomInt * score;
+    let newDiv = document.createElement("div"); //this div created when results have to show on screen
+    containerEl.appendChild(newDiv);
     console.log(randomInt, calculation)
-    containerEl.innerHTML.appendChild = "Yours Love is :" + calculation;
+    newDiv.innerHTML = "Yours Love is :" + calculation;
 })
